4a30ece87dbfab7dc1c71903e9c0506f48163402
tomttk <tualkrebstom@gmail.com>
2023-04-24 17:42:58 +0200

    Initial commit

---
+++ Source/FetchArt/RemoteImagePlane.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RemoteImagePlane.h"
+
+// Sets default values
+ARemoteImagePlane::ARemoteImagePlane()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARemoteImagePlane::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARemoteImagePlane::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
